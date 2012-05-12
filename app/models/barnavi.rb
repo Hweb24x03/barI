5aae10ca3751a3a792876298140fe3592eff58aa
ogatomo <t.ogawa.jp@gmail.com>
2012-05-12 20:01:00 +0900

    BAR-NAVIのパラメータのjsonp→jsonに修正

--- app/models/barnavi.rb
+++ app/models/barnavi.rb
@@ -4,10 +4,9 @@ require 'uri'
 class Barnavi
   # sample:
   # json = b.search(:pattern => 1, :pref => 34, :lat => 34.391911, :lng => 132.467794)
-  def search
-    query_string = params.map{|k,v|
-      URI.encode(k.to_s) + "=" + URI.encode(v.to_s)
-    }.join("&")
+  def search params
+
+    query_string = URI.encode_www_form(params)
 
     uri = URI.parse("http://webapi.suntory.co.jp")
     res = Net::HTTP.start(uri.host, uri.port) {|http|
@@ -15,7 +14,7 @@ class Barnavi
       url = URI.encode("http://example.com")
       key = BAR_NAVI_KEY
 
-      http.get("/barnavi/v2/shops?key=#{key}&url=#{url}&format=jsonp&#{query_string}")
+      http.get("/barnavi/v2/shops?key=#{key}&url=#{url}&format=json&#{query_string}")
     }
 
     res.body
