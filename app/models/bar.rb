19787b4b925b15ca2e1d4bcce711b76c4f6ca866
Tomohiko Himura <eiel.hal@gmail.com>
2012-05-13 01:02:46 +0900

    Barの必要なメソッドを追加

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,2 +1,15 @@
 class Bar < ActiveRecord::Base
+  has_many :matches, through: :going_bars
+
+  def match_num
+    self.users.count - 1
+  end
+
+  def today_num
+    1
+  end
+
+  def topic_rate
+    100
+  end
 end
