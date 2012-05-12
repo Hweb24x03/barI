9829e079ea64f1ab438ea71384a3ac56f89427f7
takashi.akagi <nisyu@mac.com>
2012-05-12 10:22:50 +0900

    初期

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Happydays::Application.config.secret_token = '14bfcbca96d49e3e75cfac086773db7b0e4f91da1aa7fd1c463f2c0658aa9227f935ef2c7940d4f9ecacd1ed7ff41d9985564c011d31c967143b0a00e8af5f1e'
