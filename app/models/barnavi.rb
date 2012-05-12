bc979fac82e13a451ca2ac2b1e12f1f01ae3022f
ogatomo <t.ogawa.jp@gmail.com>
2012-05-12 19:02:29 +0900

    net/httpでAPI呼出実装

--- app/models/barnavi.rb
+++ app/models/barnavi.rb
@@ -1,6 +1,24 @@
+require 'net/http'
+require 'uri'
+
 class Barnavi
+  # sample:
+  # json = b.search(:pattern => 1, :pref => 34, :lat => 34.391911, :lng => 132.467794)
   def search
-    "hoge"
+    query_string = params.map{|k,v|
+      URI.encode(k.to_s) + "=" + URI.encode(v.to_s)
+    }.join("&")
+
+    uri = URI.parse("http://webapi.suntory.co.jp")
+    res = Net::HTTP.start(uri.host, uri.port) {|http|
+
+      url = URI.encode("http://example.com")
+      key = BAR_NAVI_KEY
+
+      http.get("/barnavi/v2/shops?key=#{key}&url=#{url}&format=jsonp&#{query_string}")
+    }
+
+    res.body
   end
 
   def url
